6a721562544329351d3e598a2b8a7073a06cb5c7
Moritz Wilksch <moritzwilksch@gmail.com>
2022-01-10 00:58:25 +0100

    start vanilla benchmark

--- src/postgres_benchmarker.py
+++ src/postgres_benchmarker.py
@@ -9,6 +9,7 @@ import time
 from rich.console import Console
 from rich.markdown import Markdown
 from rich.table import Table
+import psycopg2
 
 c = Console()
 Base = declarative_base()
@@ -97,7 +98,77 @@ class PostgresBenchmarker:
         table.add_row("filter_names", f"{self.filter_names():.3f}")
         c.print(table)
 
+# TODO: fix this class
+class VanillaPostgresBenchmarker:
+    """ Does NOT use SQLAlchemy, only vanilla PsycoPG2 """
+
+    def __init__(self, N=10_000) -> None:
+        conn = psycopg2.connect(
+            dbname="postgres", user="postgres", host="localhost", password="postgres"
+        )
+
+        self.N = N
+        self.letters = "abcdefghijklmnopqrstuvwxyz"
+
+        cur = conn.cursor()
+        cur.execute(
+        """
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255),
+            dob TIMESTAMP
+        )
+        """)
+
+    @timeit_decorator
+    def insert_all(self) -> None:
+        users = []
+        for _ in range(self.N):
+            name = "".join(random.sample(self.letters, random.randint(4, 25)))
+            dob = datetime.datetime.now()
+            users.append({"name": name, "dob": dob})
+
+
+    @timeit_decorator
+    def insert_separately(self) -> None:
+        for _ in range(self.N):
+            name = "".join(random.sample(self.letters, random.randint(4, 25)))
+            dob = datetime.datetime.now()
+            user = User(name=name, dob=dob)
+            self.session.add(user)
+        self.session.commit()
+
+    @timeit_decorator
+    def read_all_names(self) -> None:
+        user_names = []
+        for user in self.session.query(User).all():
+            user_names.append(user.name)
+
+        printinfo(f"read_all_names: {len(user_names)} users found")
+
+    @timeit_decorator
+    def filter_names(self) -> None:
+        user_names = []
+        for user in self.session.query(User).filter(User.name.contains("mo")):
+            user_names.append(user.name)
+        printinfo(f"filter_names: {len(user_names)} users found")
+
+    def run(self):
+        c = Console()
+        c.print()
+        table = Table(title="Vanilla Postgres Benchmark")
+        table.add_column("Benchmark", justify="left")
+        table.add_column("Time", justify="center", style="cyan bold")
+
+        table.add_row("insert_all", f"{self.insert_all():.3f}")
+        table.add_row("insert_separately", f"{self.insert_separately():.3f}")
+        table.add_row("read_all_names", f"{self.read_all_names():.3f}")
+        table.add_row("filter_names", f"{self.filter_names():.3f}")
+        c.print(table)
+
 
 if __name__ == "__main__":
-    bm = PostgresBenchmarker()
-    bm.run()
+    # bm = PostgresBenchmarker()
+    # bm.run()
+
+    vbm = VanillaPostgresBenchmarker()
