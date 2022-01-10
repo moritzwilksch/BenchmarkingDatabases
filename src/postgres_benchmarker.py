956e5cfe54285b8526f39c385213245ee4fec2b4
Moritz Wilksch <moritzwilksch@gmail.com>
2022-01-10 18:15:40 +0100

    vanilla postgres

--- src/postgres_benchmarker.py
+++ src/postgres_benchmarker.py
@@ -98,60 +98,71 @@ class PostgresBenchmarker:
         table.add_row("filter_names", f"{self.filter_names():.3f}")
         c.print(table)
 
+
 # TODO: fix this class
 class VanillaPostgresBenchmarker:
     """ Does NOT use SQLAlchemy, only vanilla PsycoPG2 """
 
     def __init__(self, N=10_000) -> None:
-        conn = psycopg2.connect(
+        self.conn = psycopg2.connect(
             dbname="postgres", user="postgres", host="localhost", password="postgres"
         )
 
         self.N = N
         self.letters = "abcdefghijklmnopqrstuvwxyz"
 
-        cur = conn.cursor()
+        cur = self.conn.cursor()
+        cur.execute("DROP TABLE IF EXISTS users")
         cur.execute(
-        """
+            """
         CREATE TABLE IF NOT EXISTS users (
             id SERIAL PRIMARY KEY,
             name VARCHAR(255),
             dob TIMESTAMP
         )
-        """)
+        """
+        )
+        self.conn.commit()
 
     @timeit_decorator
     def insert_all(self) -> None:
+        cur = self.conn.cursor()
+
         users = []
         for _ in range(self.N):
             name = "".join(random.sample(self.letters, random.randint(4, 25)))
             dob = datetime.datetime.now()
-            users.append({"name": name, "dob": dob})
-
+            users.append((name, dob))
+        
+        cur.executemany("INSERT INTO users (name, dob) VALUES (%s, %s)", users)    
+        self.conn.commit()
 
     @timeit_decorator
     def insert_separately(self) -> None:
+        cur = self.conn.cursor()
         for _ in range(self.N):
             name = "".join(random.sample(self.letters, random.randint(4, 25)))
             dob = datetime.datetime.now()
-            user = User(name=name, dob=dob)
-            self.session.add(user)
-        self.session.commit()
+            cur.execute("INSERT INTO users (name, dob) VALUES (%s, %s)", (name, dob))
+        self.conn.commit()
 
     @timeit_decorator
     def read_all_names(self) -> None:
+        cur = self.conn.cursor()
         user_names = []
-        for user in self.session.query(User).all():
-            user_names.append(user.name)
-
+        cur.execute("SELECT * FROM users;")
+        for user in cur.fetchall():
+            user_names.append(user[1])
         printinfo(f"read_all_names: {len(user_names)} users found")
 
     @timeit_decorator
     def filter_names(self) -> None:
+        cur = self.conn.cursor()
         user_names = []
-        for user in self.session.query(User).filter(User.name.contains("mo")):
-            user_names.append(user.name)
-        printinfo(f"filter_names: {len(user_names)} users found")
+        cur.execute("SELECT * FROM users WHERE users.name LIKE '%mo%';")
+        for user in cur.fetchall():
+            user_names.append(user[1])
+        printinfo(f"read_all_names: {len(user_names)} users found")
 
     def run(self):
         c = Console()
@@ -168,7 +179,8 @@ class VanillaPostgresBenchmarker:
 
 
 if __name__ == "__main__":
-    # bm = PostgresBenchmarker()
-    # bm.run()
+    bm = PostgresBenchmarker()
+    bm.run()
 
-    vbm = VanillaPostgresBenchmarker()
+    # vbm = VanillaPostgresBenchmarker()
+    # vbm.run()
